76ea817cc562ee8d2b0adea3c922638eff9bc7bb
github username <github email address>
2019-11-13 16:16:00 +0000

    buy trends

--- src/containers/UserPortfolio.js
+++ src/containers/UserPortfolio.js
@@ -2,17 +2,28 @@ import React from 'react'
 import '../css/UserPortfolio.css'
 import OwnedTrend from '../components/OwnedTrend'
 
+
 class UserPortfolio extends React.PureComponent {
+
+    state={
+        initialValue: 0,
+        currentValue: 0
+    }
+
     render() {
         return(
             <div className="user-portfolio">
                 <h1>Owned stock</h1>
                 <div className="owned-trend-box">
-                {this.props.ownedTrends.map((trend, index) => (
-                    < OwnedTrend key={index}
+                {this.props.ownedTrends.map((trend, index) => {
+
+                    this.setState({initialValue: this.state.initialValue+=trend.initial_valuation})
+                    this.setState({currentValue: this.state.currentValue+=trend.current_valuation})
+
+                   return < OwnedTrend key={index}
                                  trend={trend}
                                                 />
-                ))}
+                })}
                 </div>
             </div>
         )
--- src/helpers/API.js
+++ src/helpers/API.js
@@ -1,5 +1,6 @@
 const BASE_URL = 'http://localhost:3000'
 const TRENDS = `${BASE_URL}/trends`
+const OWNEDTRENDS = `${BASE_URL}/owned_trends`
 const USERS = `${BASE_URL}/users`
 const USER_LOGIN = `${BASE_URL}/login`
 
@@ -8,15 +9,29 @@ const getTrends = () => fetch(TRENDS).then(resp => resp.json())
 
 
 //BUY TRENDS
-const buyTrends = trendDetails => fetch(TRENDS, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-    },
-    body: JSON.stringify(trendDetails)
+const buyTrends = (trendDetails, userId, quantity) => {
+    
+    fetch(OWNEDTRENDS, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            trend: {
+            name: trendDetails.name,
+            query: trendDetails.query,
+            url: trendDetails.url,
+            user_id: userId,
+            initial_rank: trendDetails.rank,
+            initial_tweet_volume: trendDetails.tweet_volume,
+            initial_valuation: 1,
+            quantity_trends_purchased: quantity
+            }
+        })
     }
 ).then(resp => resp.json())
+}
 
 
 //CREATE NEW USER
@@ -42,19 +57,12 @@ const loginUser = userDetails => fetch(USER_LOGIN, {
     body: JSON.stringify(userDetails)
 }).then(resp => resp.json())
 
-// {
-//     name: ,
-//     url: ,
-//     tweet_volume: , 
-//     user_id:  , 
-//     initial_rank: [rank],
-//     current_rank: [rank],
-//     valuation: 1,
-// }
+const getUserPortfolio = (user_id) => fetch(`${BASE_URL}/users/${user_id}/portfolio`).then(resp => resp.json())
 
 export default {
     getTrends,
     buyTrends,
     createUser,
-    loginUser
+    loginUser,
+    getUserPortfolio
 }
